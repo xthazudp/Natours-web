97f40b1d91f2fef7e097a0ab10b06e8a93654651
xthazudp <xthazudp@gmail.com>
2020-10-15 17:57:02 +0545

    commit

--- server.js
+++ server.js
@@ -16,8 +16,48 @@ mongoose
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
+    useUnifiedTopology: true,
   })
   .then(() => console.log('DB connection successful'));
+
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price'],
+  },
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
+
+// const testTour = new Tour({
+//   name: 'The Forest Hiker',
+//   rating: 4.7,
+//   price: 497,
+// });
+
+const testTour = new Tour({
+  name: 'The Park Carper',
+  rating: 4.5,
+  price: 600,
+});
+
+testTour
+  .save()
+  .then((doc) => {
+    console.log(doc);
+  })
+  .catch((err) => {
+    console.log('ERROR:', err);
+  });
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
