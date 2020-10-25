a308e687e2f86cced638e0cf281de5b0612bec88
xthazudp <xthazudp@gmail.com>
2020-10-25 20:48:31 +0545

    refactoring for mvc

--- server.js
+++ server.js
@@ -20,23 +20,8 @@ mongoose
   })
   .then(() => console.log('DB connection successful'));
 
-const tourSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'A tour must have a name'],
-    unique: true,
-  },
-  rating: {
-    type: Number,
-    default: 4.5,
-  },
-  price: {
-    type: Number,
-    required: [true, 'A tour must have a price'],
-  },
-});
 
-const Tour = mongoose.model('Tour', tourSchema);
+////// FOR TESTING PURPOSE ONLY ///////
 
 // const testTour = new Tour({
 //   name: 'The Forest Hiker',
@@ -44,20 +29,28 @@ const Tour = mongoose.model('Tour', tourSchema);
 //   price: 497,
 // });
 
-const testTour = new Tour({
-  name: 'The Park Carper',
-  rating: 4.5,
-  price: 600,
-});
+// const testTour = new Tour({
+//   name: 'The Park Carper',
+//   rating: 4.5,
+//   price: 600,
+// });
 
-testTour
-  .save()
-  .then((doc) => {
-    console.log(doc);
-  })
-  .catch((err) => {
-    console.log('ERROR:', err);
-  });
+// const testTour = new Tour({
+//   name: 'The Snow Adventure',
+//   rating: 4.8,
+//   price: 900,
+// });
+
+// testTour
+//   .save()
+//   .then((doc) => {
+//     console.log(doc);
+//   })
+//   .catch((err) => {
+//     console.log('ERROR:', err);
+//   });
+
+//////////////////////////
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
