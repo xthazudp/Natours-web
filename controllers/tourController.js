e13443eb1af29edb6249f2b9a4863597564e108c
xthazudp <xthazudp@gmail.com>
2020-11-03 07:54:25 +0545

    Making the API better filtering

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,3 +1,4 @@
+const { query } = require('express');
 const Tour = require('./../models/tourModel');
 
 // const tours = JSON.parse(
@@ -10,8 +11,25 @@ exports.getAllTours = async (req, res) => {
   // console.log(req.requestTime);
 
   try{
-    const tours = await Tour.find();
+    // BUILD THE QUERY
+    const queryObj = {...req.query};
+    const excludedFields = ['page', 'sort', 'limit', 'fields'];
+    excludedFields.forEach(el => delete queryObj[el]);
 
+    // console.log(req.query, queryObj);
+    const query = Tour.find(queryObj);
+
+    // const query = Tour.find({
+    //   duration : 5,
+    //   difficulty: 'easy'
+    // });
+
+    // const query = Tour.find().where('duration').equals(5).where('difficulty').equals('easy');
+
+    // EXECUTE THE QUERY
+    const tours = await query;
+
+    // SEND RESPONSE
     res.status(200).json({
       status: 'success',
       // requestedAt: req.requestTime,
