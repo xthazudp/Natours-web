37b5a97aadc85a0d736526f91ed6f1200199729a
xthazudp <xthazudp@gmail.com>
2021-02-05 22:59:53 +0545

    refactoring api features

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,5 +1,6 @@
 const { query } = require('express');
 const Tour = require('./../models/tourModel');
+const APIFeatures = require('./../utlis/apiFeatures');
 
 exports.aliasTopTours = (req, res, next) => {
   req.query.limit = '5';
@@ -7,74 +8,16 @@ exports.aliasTopTours = (req, res, next) => {
   req.query.fields = 'name, price, ratingsAverage, summary, difficulty';
   next();
 };
-// const tours = JSON.parse(
-//   fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
-// );
-
-
 
 exports.getAllTours = async (req, res) => {
   // console.log(req.requestTime);
 
   try{
     console.log(req.query);
-    
-    // BUILD THE QUERY
-    // 1.a) Filtering
-    const queryObj = {...req.query};
-    const excludedFields = ['page', 'sort', 'limit', 'fields'];
-    excludedFields.forEach(el => delete queryObj[el]);
-
-    // 1.b) Advance Filtering
-    // console.log(req.query, queryObj);
-    let queryStr = JSON.stringify(queryObj);
-    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
-
-    // {dificulty: 'easy', duration:{ $gte: 5} }
-    // { difficulty: 'easy', duration: { gte: '5' } }
-    // gte, gt, lte, lt
-
-    // const query = Tour.find({
-    //   duration : 5,
-    //   difficulty: 'easy'
-    // });
-
-    // const query = Tour.find().where('duration').equals(5).where('difficulty').equals('easy');
-
-    let query = Tour.find(JSON.parse(queryStr));
-
-    // 2) Sorting
-
-    if(req.query.sort){
-      const sortBy = req.query.sort.split(',').join(' ');
-      query = query.sort(sortBy)
-    }else{
-      query = query.sort('-createdAt');
-    }
-
-    // 3) Field Limiting
-
-    if(req.query.fields){
-      const fields = req.query.fields.split(',').join(' ');
-      query = query.select(fields);
-    }else{
-      query = query.select('-__v');
-    }
-
-    // 4) Pagination
-
-    const page = req.query.page * 1 || 1;
-    const limit = req.query.limit * 1 || 100;
-    const skip = (page - 1) * limit;
-    query = query.skip(skip).limit(limit);
-
-    if (req.query.page){
-      const numTours = await Tour.countDocuments();
-      if(skip >= numTours) throw new Error('This Page does not exit!!!');
-    }
 
     // EXECUTE THE QUERY
-    const tours = await query;
+    const features = new APIFeatures(Tour.find(), req.query).filter().sort().limitFields().paginate();
+    const tours = await features.query;
 
     // SEND RESPONSE
     res.status(200).json({
