309595f26cef688d0cd4243168424cb0093b6f86
xthazudp <xthazudp@gmail.com>
2021-02-10 16:38:26 +0545

    data validation custom validators

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
+const validator = require('validator');
 
 const tourSchema = new mongoose.Schema({
     name: {
@@ -9,6 +10,7 @@ const tourSchema = new mongoose.Schema({
       trim: true,
       maxlength: [40, 'A tour name must have less or equal then 40 characters'],
       minlength: [10, 'A tour name must have more or equal then 40 characters']
+      // validate: [validator.isAlpha, 'A tour name must only contain character']
     },
     slug: String,
     duration:{
@@ -41,7 +43,16 @@ const tourSchema = new mongoose.Schema({
       type: Number,
       required: [true, 'A tour must have a price'],
     },
-    priceDiscount: Number,
+    priceDiscount: {
+      type: Number,
+      validate: {
+        validator:function(val){
+          // this only points to current doc on NEW document creation
+          return val < this.price;
+        },
+        messages: 'Discount price ({VALUE}) should be below regular price'
+      } 
+    }, 
     summary:{
       type: String,
       trim: true,
